4a8f2938ce6b97df1237af19f06967e17771f1b0
Sehyeon Youn <dev.younlab@gmail.com>
2018-12-20 20:59:33 +0900

    [UPDATE] user models 정의

--- backend-project-cookiecutter/nomadgram/config/settings/base.py
+++ backend-project-cookiecutter/nomadgram/config/settings/base.py
@@ -24,7 +24,7 @@ DEBUG = env.bool('DJANGO_DEBUG', False)
 # In Windows, this must be set to your system time zone.
 TIME_ZONE = 'Asia/Seoul'
 # https://docs.djangoproject.com/en/dev/ref/settings/#language-code
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 # https://docs.djangoproject.com/en/dev/ref/settings/#site-id
 SITE_ID = 1
 # https://docs.djangoproject.com/en/dev/ref/settings/#use-i18n
@@ -39,7 +39,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 DATABASES = {
-    'default': env.db('DATABASE_URL', default='postgres:///copygram')
+    'default': env.db('DATABASE_URL', default='postgres:///nomadgram')
 }
 DATABASES['default']['ATOMIC_REQUESTS'] = True
 
--- backend-project-cookiecutter/nomadgram/nomadgram/users/models.py
+++ backend-project-cookiecutter/nomadgram/nomadgram/users/models.py
@@ -1,14 +1,24 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 from django.db.models import CharField
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
 
 class User(AbstractUser):
-
     # First Name and Last Name do not cover name patterns
     # around the globe.
+    GENDER_CHOICES = (
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('not-specified', 'Not specified'),
+    )
+
     name = CharField(_("Name of User"), blank=True, max_length=255)
+    website = models.URLField(null=True, blank=True)
+    bio = models.TextField(null=True)
+    phone = models.CharField(max_length=140, null=True)
+    gender = models.CharField(max_length=80, choices=GENDER_CHOICES, null=True)
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
