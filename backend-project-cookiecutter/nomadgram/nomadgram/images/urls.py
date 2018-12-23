32954ba1fa9972321e7272ecaef66ce3a42ee5cd
Sehyeon Youn <dev.younlab@gmail.com>
2018-12-24 02:04:16 +0900

    [UPDATE] images comment create api

--- backend-project-cookiecutter/nomadgram/nomadgram/images/urls.py
+++ backend-project-cookiecutter/nomadgram/nomadgram/images/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'images'
 urlpatterns = [
     path('', views.Feed.as_view(), name='feed'),
-    path('<int:image_id>/like/', views.LikeImage.as_view(), name='like_image')
+    path('<int:image_id>/like/', views.LikeImage.as_view(), name='like_image'),
+    path('<int:image_id>/comment/', views.CommentOnImage.as_view(), name='comment')
 ]
