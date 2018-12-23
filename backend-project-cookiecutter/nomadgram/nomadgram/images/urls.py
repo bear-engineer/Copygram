ac7ee9c7a6a9beaa2b4b453c8e23b7c84b9765fb
Sehyeon Youn <dev.younlab@gmail.com>
2018-12-24 03:55:46 +0900

    [UPDATE] like api 분리

--- backend-project-cookiecutter/nomadgram/nomadgram/images/urls.py
+++ backend-project-cookiecutter/nomadgram/nomadgram/images/urls.py
@@ -5,6 +5,7 @@ app_name = 'images'
 urlpatterns = [
     path('', views.Feed.as_view(), name='feed'),
     path('<int:image_id>/likes/', views.LikeImage.as_view(), name='like_image'),
-    path('<int:image_id>/comments/', views.CommentOnImage.as_view(), name='comment_image'),
+    path('<int:image_id>/unlikes/', views.UnLikeImage.as_view(), name='unlike_image'),
+    path('<int:image_id>/comments/', views.CommentOnImage.as_view(), name='comment_create'),
     path('comments/<int:comment_id>/', views.Comment.as_view(), name='comment_delete')
 ]
---
+++ backend-project-cookiecutter/nomadgram/nomadgram/users/serializers.py
@@ -0,0 +1,14 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class ExploreUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'profile_image',
+            'username',
+            'name'
+        )
