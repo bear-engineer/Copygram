db635920b1d69a14f97b5e6a1a91d551af07433d
Sehyeon Youn <dev.younlab@gmail.com>
2019-01-08 01:01:48 +0900

    [ADD] notification app create

--- backend-project-cookiecutter/nomadgram/config/settings/base.py
+++ backend-project-cookiecutter/nomadgram/config/settings/base.py
@@ -73,6 +73,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     'nomadgram.users.apps.UsersAppConfig',
     'nomadgram.images.apps.ImagesConfig',
+    'nomadgram.notifications.apps.NotificationsConfig'
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
---
+++ backend-project-cookiecutter/nomadgram/nomadgram/notifications/models.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+from nomadgram.images import models as image_models
+
+from nomadgram.users import models as user_models
+
+
+class Notification(image_models.TimeStampedModel):
+    TYPE_CHOICES = (
+        ('like', 'Like'),
+        ('comment', 'Comment'),
+        ('follow', 'Follow'),
+    )
+
+    creator = models.ForeignKey(user_models.User, on_delete=models.CASCADE, related_name='creator')
+    to = models.ForeignKey(user_models.User, on_delete=models.CASCADE, related_name='to')
+    notifications_type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    image = models.ForeignKey(image_models.Image, on_delete=models.SET_NULL, null=True, blank=True)
