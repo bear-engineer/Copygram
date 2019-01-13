1140f1a5881250406dd5f8c8dc0b15b2c4d567de
Sehyeon Youn <dev.younlab@gmail.com>
2019-01-14 02:35:34 +0900

    [UPDATE] add upload photo

--- backend-project-cookiecutter/nomadgram/config/urls.py
+++ backend-project-cookiecutter/nomadgram/config/urls.py
@@ -3,7 +3,6 @@ from django.urls import include, path
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views import defaults as default_views
-from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
                   path('rest-auth/', include('rest_auth.urls')),
