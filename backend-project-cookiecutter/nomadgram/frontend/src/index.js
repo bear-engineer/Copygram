4dcfa1116dc0f936cdd3fd41083412828922ae07
Sehyeon Youn <dev.younlab@gmail.com>
2019-01-16 05:12:11 +0900

    [UPDATE] connected-react-router setting, history setting

--- backend-project-cookiecutter/nomadgram/frontend/src/index.js
+++ backend-project-cookiecutter/nomadgram/frontend/src/index.js
@@ -1,22 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.scss";
-import App from "./App";
+import App from "App";
 import { Provider } from "react-redux";
-import store from "./redux/configureStore";
-// import * as serviceWorker from "./serviceWorker";
-
-// user reducer -> create store & reducer combine -> export store
-// App provider setting
+import { ConnectedRouter } from "connected-react-router";
+import store, { history } from "redux/configureStore";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
   </Provider>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-// serviceWorker.unregister();
--- backend-project-cookiecutter/nomadgram/frontend/src/redux/configureStore.js
+++ backend-project-cookiecutter/nomadgram/frontend/src/redux/configureStore.js
@@ -1,21 +1,31 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { combineReducers, createStore, applyMiddleware, compose } from "redux";
+import { connectRouter, routerMiddleware } from "connected-react-router";
+import createHistory from "history/createBrowserHistory";
 import thunk from "redux-thunk";
 import users from "redux/modules/users";
 
-const middlewares = [thunk];
-
 const env = process.env.NODE_ENV;
+
+const history = createHistory();
+
+const middlewares = [thunk, routerMiddleware(history)];
 
 if (env === "development") {
   const { logger } = require("redux-logger");
   middlewares.push(logger);
 }
 
-const reducre = combineReducers({
-  users
+const reducer = combineReducers({
+  users,
+  router: connectRouter(history)
 });
 
 let store = initialState =>
-  createStore(reducre, applyMiddleware(...middlewares));
+  createStore(
+    connectRouter(history)(reducer),
+    compose(applyMiddleware(...middlewares))
+  );
+
+export { history };
 
 export default store();
