241eb0583229ec82c46d9bffbba1d4ab82ff0071
Sehyeon Youn <dev.younlab@gmail.com>
2019-01-15 22:07:22 +0900

    [UPDATE] redux-logger setting, dev middleware

--- backend-project-cookiecutter/nomadgram/frontend/src/index.js
+++ backend-project-cookiecutter/nomadgram/frontend/src/index.js
@@ -6,6 +6,9 @@ import { Provider } from "react-redux";
 import store from "./redux/configureStore";
 // import * as serviceWorker from "./serviceWorker";
 
+// user reducer -> create store & reducer combine -> export store
+// App provider setting
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
@@ -17,4 +20,3 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 // serviceWorker.unregister();
-localStorage.setItem("baseCourse", "nomad academy");
--- backend-project-cookiecutter/nomadgram/frontend/src/redux/configureStore.js
+++ backend-project-cookiecutter/nomadgram/frontend/src/redux/configureStore.js
@@ -1,10 +1,21 @@
-import { createStore, combineReducers } from "redux";
-import users from "./modules/users";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import users from "redux/modules/users";
+
+const middlewares = [thunk];
+
+const env = process.env.NODE_ENV;
+
+if (env === "development") {
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
+}
 
 const reducre = combineReducers({
   users
 });
 
-let store = initialState => createStore(reducre);
+let store = initialState =>
+  createStore(reducre, applyMiddleware(...middlewares));
 
 export default store();
