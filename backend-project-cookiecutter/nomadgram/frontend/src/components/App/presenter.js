e5c2392edb4eaf429893dfcc2be8c4375bb94bde
Sehyeon Youn <dev.younlab@gmail.com>
2019-01-18 01:14:39 +0900

    [UPDATE] public route, privat route setting

--- backend-project-cookiecutter/nomadgram/frontend/src/components/App/presenter.js
+++ backend-project-cookiecutter/nomadgram/frontend/src/components/App/presenter.js
@@ -1,14 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 import styles from "./styles.scss";
 import Footer from "components/Footer";
-class App extends Component {
-  render() {
-    return (
-      <div className={styles.App}>
-        <Footer />
-      </div>
-    );
-  }
-}
+import { Route, Switch } from "react-router-dom";
+
+const App = props => [
+  props.isLoggedIn ? <PrivateRoute key={2} /> : <PublicRoutes key={3} />,
+  <Footer key={4} />
+];
+
+const PrivateRoute = props => (
+  <Switch>
+    <Route exact path="/" render={() => "feed"} />
+    <Route exact path="/explore" render={() => "explore"} />
+  </Switch>
+);
+
+const PublicRoutes = props => (
+  <Switch>
+    <Route exact path="/" render={() => "login"} />
+    <Route exact path="/forgot" render={() => "password"} />
+  </Switch>
+);
 
 export default App;
